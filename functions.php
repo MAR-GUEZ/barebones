bcea635e024f51f0048f57dc15a02250a3f532ac
MARLMG <152422770+MAR-GUEZ@users.noreply.github.com>
2024-08-20 12:07:53 -0500

    Delete line that deregisters jquery. Probably need to find out why this was inplace

--- functions.php
+++ functions.php
@@ -11,13 +11,13 @@ require_once 'includes/custom-functions.php';
  * Add support for useful stuff
  */
 
-if ( function_exists( 'add_theme_support' ) ) {
+if (function_exists('add_theme_support')) {
 
     // Add support for document title tag
-    add_theme_support( 'title-tag' );
+    add_theme_support('title-tag');
 
     // Add Thumbnail Theme Support
-    add_theme_support( 'post-thumbnails' );
+    add_theme_support('post-thumbnails');
     // add_image_size( 'custom-size', 700, 200, true );
 
     // Add Support for post formats
@@ -25,7 +25,7 @@ if ( function_exists( 'add_theme_support' ) ) {
     // add_post_type_support( 'page', 'excerpt' );
 
     // Localisation Support
-    load_theme_textdomain( 'barebones', get_template_directory() . '/languages' );
+    load_theme_textdomain('barebones', get_template_directory() . '/languages');
 }
 
 
@@ -33,7 +33,7 @@ if ( function_exists( 'add_theme_support' ) ) {
  * Hide admin bar
  */
 
- add_filter( 'show_admin_bar', '__return_false' );
+add_filter('show_admin_bar', '__return_false');
 
 
 /**
@@ -57,7 +57,8 @@ remove_action('wp_print_styles', 'print_emoji_styles');
  * @return void
  */
 
-function barebones_post_comments_feed_link() {
+function barebones_post_comments_feed_link()
+{
     return;
 }
 
@@ -68,15 +69,18 @@ add_filter('post_comments_feed_link', 'barebones_post_comments_feed_link');
  * Enqueue scripts
  */
 
-function barebones_enqueue_scripts() {
+function barebones_enqueue_scripts()
+{
     // wp_enqueue_style( 'fonts', '//fonts.googleapis.com/css?family=Font+Family' );
     // wp_enqueue_style( 'icons', '//use.fontawesome.com/releases/v5.0.10/css/all.css' );
-    wp_deregister_script('jquery');
-    wp_enqueue_style( 'styles', get_stylesheet_directory_uri() . '/style.css?' . filemtime( get_stylesheet_directory() . '/style.css' ) );
-    wp_enqueue_script( 'scripts', get_stylesheet_directory_uri() . '/js/scripts.min.js?' . filemtime( get_stylesheet_directory() . '/js/scripts.min.js' ), [], null, true );
+    /* THIS MESSES WITH WP BAKERY
+  wp_deregister_script('jquery');
+  /* THIS MESSES WITH WP BAKERY*/
+    wp_enqueue_style('styles', get_stylesheet_directory_uri() . '/style.css?' . filemtime(get_stylesheet_directory() . '/style.css'));
+    wp_enqueue_script('scripts', get_stylesheet_directory_uri() . '/js/scripts.min.js?' . filemtime(get_stylesheet_directory() . '/js/scripts.min.js'), [], null, true);
 }
 
-add_action( 'wp_enqueue_scripts', 'barebones_enqueue_scripts' );
+add_action('wp_enqueue_scripts', 'barebones_enqueue_scripts');
 
 
 /**
@@ -88,22 +92,23 @@ add_action( 'wp_enqueue_scripts', 'barebones_enqueue_scripts' );
  * @return void
  */
 
-function defer_scripts( $tag, $handle, $src ) {
+function defer_scripts($tag, $handle, $src)
+{
 
-	// The handles of the enqueued scripts we want to defer
-	$defer_scripts = [
+    // The handles of the enqueued scripts we want to defer
+    $defer_scripts = [
         'SCRIPT_ID'
     ];
 
     // Find scripts in array and defer
-    if ( in_array( $handle, $defer_scripts ) ) {
+    if (in_array($handle, $defer_scripts)) {
         return '<script type="text/javascript" src="' . $src . '" defer="defer"></script>' . "\n";
     }
-    
-    return $tag;
-} 
 
-add_filter( 'script_loader_tag', 'defer_scripts', 10, 3 );
+    return $tag;
+}
+
+add_filter('script_loader_tag', 'defer_scripts', 10, 3);
 
 
 /**
@@ -112,28 +117,30 @@ add_filter( 'script_loader_tag', 'defer_scripts', 10, 3 );
  * @return string
  */
 
-function add_gtag_to_head() {
+function add_gtag_to_head()
+{
 
     // Check is staging environment
-    if ( strpos( get_bloginfo( 'url' ), '.test' ) !== false ) return;
+    if (strpos(get_bloginfo('url'), '.test') !== false)
+        return;
 
     // Google Analytics
     $tracking_code = 'UA-*********-1';
-    
-    ?>
-        <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo $tracking_code; ?>"></script>
-        <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
 
-            gtag('config', '<?php echo $tracking_code; ?>');
-        </script>
+    ?>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo $tracking_code; ?>"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag() { dataLayer.push(arguments); }
+        gtag('js', new Date());
+
+        gtag('config', '<?php echo $tracking_code; ?>');
+    </script>
     <?php
 }
 
-add_action( 'wp_head', 'add_gtag_to_head' );
+add_action('wp_head', 'add_gtag_to_head');
 
 
 
@@ -143,11 +150,12 @@ add_action( 'wp_head', 'add_gtag_to_head' );
  * @return void
  */
 
-function deregister_scripts() {
-    wp_deregister_script( 'wp-embed' );
+function deregister_scripts()
+{
+    wp_deregister_script('wp-embed');
 }
 
-add_action( 'wp_footer', 'deregister_scripts' );
+add_action('wp_footer', 'deregister_scripts');
 
 
 /**
@@ -156,11 +164,12 @@ add_action( 'wp_footer', 'deregister_scripts' );
  * @return void
  */
 
-function deregister_styles() {
-    wp_dequeue_style( 'wp-block-library' );
+function deregister_styles()
+{
+    wp_dequeue_style('wp-block-library');
 }
 
-add_action( 'wp_print_styles', 'deregister_styles', 100 );
+add_action('wp_print_styles', 'deregister_styles', 100);
 
 
 /**
@@ -169,14 +178,15 @@ add_action( 'wp_print_styles', 'deregister_styles', 100 );
  * @return void
  */
 
-function barebones_register_nav_menus() {
+function barebones_register_nav_menus()
+{
     register_nav_menus([
         'header' => 'Header',
         'footer' => 'Footer',
     ]);
 }
 
-add_action( 'after_setup_theme', 'barebones_register_nav_menus', 0 );
+add_action('after_setup_theme', 'barebones_register_nav_menus', 0);
 
 
 /**
@@ -186,7 +196,8 @@ add_action( 'after_setup_theme', 'barebones_register_nav_menus', 0 );
  * @return void
  */
 
-function barebones_nav_menu_args( $args ) {
+function barebones_nav_menu_args($args)
+{
     $args['container'] = false;
     $args['container_class'] = false;
     $args['menu_id'] = false;
@@ -206,10 +217,11 @@ add_filter('wp_nav_menu_args', 'barebones_nav_menu_args');
  * @return void
  */
 
-function barebones_button_shortcode( $atts, $content = null ) {
+function barebones_button_shortcode($atts, $content = null)
+{
     $atts['class'] = isset($atts['class']) ? $atts['class'] : 'btn';
     $atts['target'] = isset($atts['target']) ? $atts['target'] : '_self';
-    return '<a class="' . $atts['class'] . '" href="' . $atts['link'] . '" target="'. $atts['target'] . '">' . $content . '</a>';
+    return '<a class="' . $atts['class'] . '" href="' . $atts['link'] . '" target="' . $atts['target'] . '">' . $content . '</a>';
 }
 
 add_shortcode('button', 'barebones_button_shortcode');
@@ -222,8 +234,9 @@ add_shortcode('button', 'barebones_button_shortcode');
  * @return void
  */
 
-function barebones_mce_buttons_2( $buttons ) {
-    array_unshift( $buttons, 'styleselect' );
+function barebones_mce_buttons_2($buttons)
+{
+    array_unshift($buttons, 'styleselect');
     $buttons[] = 'hr';
 
     return $buttons;
@@ -239,41 +252,42 @@ add_filter('mce_buttons_2', 'barebones_mce_buttons_2');
  * @return void
  */
 
-function barebones_tiny_mce_before_init( $settings ) {
+function barebones_tiny_mce_before_init($settings)
+{
     $style_formats = [
         [
             'title' => 'Text Sizes',
             'items' => [
                 [
-                    'title'    => '2XL',
+                    'title' => '2XL',
                     'selector' => 'span, p',
-                    'classes'  => 'text-2xl'
+                    'classes' => 'text-2xl'
                 ],
                 [
-                    'title'    => 'XL',
+                    'title' => 'XL',
                     'selector' => 'span, p',
-                    'classes'  => 'text-xl'
+                    'classes' => 'text-xl'
                 ],
                 [
-                    'title'    => 'LG',
+                    'title' => 'LG',
                     'selector' => 'span, p',
-                    'classes'  => 'text-lg'
+                    'classes' => 'text-lg'
                 ],
                 [
-                    'title'    => 'MD',
+                    'title' => 'MD',
                     'selector' => 'span, p',
-                    'classes'  => 'text-md'
+                    'classes' => 'text-md'
                 ],
                 [
-                    'title'    => 'SM',
+                    'title' => 'SM',
                     'selector' => 'span, p',
-                    'classes'  => 'text-sm'
+                    'classes' => 'text-sm'
                 ],
                 [
-                    'title'    => 'XD',
+                    'title' => 'XD',
                     'selector' => 'span, p',
-                    'classes'  => 'text-xs'
-                ],                
+                    'classes' => 'text-xs'
+                ],
             ]
         ]
     ];
@@ -296,13 +310,16 @@ add_filter('tiny_mce_before_init', 'barebones_tiny_mce_before_init');
  * @return void
  */
 
-function get_post_thumbnail_url( $size = 'full', $post_id = false, $icon = false ) {
-    if ( ! $post_id ) {
+function get_post_thumbnail_url($size = 'full', $post_id = false, $icon = false)
+{
+    if (!$post_id) {
         $post_id = get_the_ID();
     }
 
     $thumb_url_array = wp_get_attachment_image_src(
-        get_post_thumbnail_id( $post_id ), $size, $icon
+        get_post_thumbnail_id($post_id),
+        $size,
+        $icon
     );
     return $thumb_url_array[0];
 }
@@ -312,15 +329,16 @@ function get_post_thumbnail_url( $size = 'full', $post_id = false, $icon = false
  * Add Front Page edit link to admin Pages menu
  */
 
-function front_page_on_pages_menu() {
+function front_page_on_pages_menu()
+{
     global $submenu;
-    if ( get_option( 'page_on_front' ) ) {
-        $submenu['edit.php?post_type=page'][501] = array( 
-            __( 'Front Page', 'barebones' ), 
-            'manage_options', 
-            get_edit_post_link( get_option( 'page_on_front' ) )
-        ); 
+    if (get_option('page_on_front')) {
+        $submenu['edit.php?post_type=page'][501] = array(
+            __('Front Page', 'barebones'),
+            'manage_options',
+            get_edit_post_link(get_option('page_on_front'))
+        );
     }
 }
 
-add_action( 'admin_menu' , 'front_page_on_pages_menu' );
+add_action('admin_menu', 'front_page_on_pages_menu');
